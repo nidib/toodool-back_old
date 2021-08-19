7c8cc4e31bcdb18fe77529b19c0cd066fc77ac7d
Richard Bidin <richardbidin@outlook.com>
2021-08-18 22:26:36 -0300

    Diretorio de constantes com errorTypes

---
+++ src/constants/errorTypes.js
@@ -0,0 +1,19 @@
+module.exports = [{
+	message: 'Something went wrong',
+	statusCode: 500
+}, {
+	message: 'Unauthorized',
+	statusCode: 401
+}, {
+	message: 'Login already in use',
+	statusCode: 409
+}, {
+	message: 'Validation error',
+	statusCode: 403
+}, {
+	message: 'Not enough credentials',
+	statusCode: 403
+}, {
+	message: 'Wrong username and/or password',
+	statusCode: 403
+}];
--- src/instances/CustomError.js
+++ src/instances/CustomError.js
@@ -1,3 +1,5 @@
+const errorTypes = require('../constants/errorTypes');
+
 class CustomError {
 	constructor(initialMessage, req) {
 		this.initialMessage = initialMessage;
@@ -9,26 +11,6 @@ class CustomError {
 	}
 
 	getError() {
-		const errorTypes = [{
-			message: 'Something went wrong',
-			statusCode: 500
-		}, {
-			message: 'Unauthorized',
-			statusCode: 401
-		}, {
-			message: 'Login already in use',
-			statusCode: 409
-		}, {
-			message: 'Validation error',
-			statusCode: 403
-		}, {
-			message: 'Not enough credentials',
-			statusCode: 403
-		}, {
-			message: 'Wrong username and/or password',
-			statusCode: 403
-		}];
-
 		if (this.initialMessage === 'Not Found') {
 			return {
 				message: `Not Found ${this.originalUrl}`,
