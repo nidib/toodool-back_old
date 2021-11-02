758e0f32f9d6415aa4c533d65ef1b0fe6159f4bf
Richard Bidin <richardbidin@outlook.com>
2021-11-02 12:33:57 -0300

    Tables.sql update

--- README.md
+++ README.md
@@ -12,5 +12,5 @@ Backend of a sample full stack to-do app - written in node.js
 Server Language:
 - Javascript with node.js
 
-Database
+Database:
 - PostgreSQL
--- tables.sql
+++ tables.sql
@@ -1,17 +1,33 @@
+-- IMPORT UUID FUNCTION
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- CREATE SCHEMA
+CREATE SCHEMA "db";
+
+-- USER TYPES TABLE
+CREATE TABLE "db"."user_type" (
+	"id" uuid DEFAULT uuid_generate_v4(),
+	"type" VARCHAR(10) NOT NULL UNIQUE,
+
+	PRIMARY KEY ("id")
+);
+
 -- USER WITH BASIC LOGIN INFO
-CREATE TABLE "user" (
+CREATE TABLE "db"."user" (
 	"id" uuid DEFAULT uuid_generate_v4(),
 	"username" VARCHAR(255) UNIQUE NOT NULL,
 	"password" VARCHAR(255) NOT NULL,
+	"user_type_id" uuid NOT NULL
 	
 	"created_at" TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
 	"updated_at" TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
 
 	PRIMARY KEY ("id")
+	FOREIGN KEY ("user_type_id") REFERENCES "db"."user_type" ("id") ON DELETE CASCADE
 );
 
--- USER INFO WITH MORE DATA ON USER
-CREATE TABLE "user_info" (
+-- USER INFO
+CREATE TABLE "db"."user_info" (
 	"id" uuid DEFAULT uuid_generate_v4(),
 	"first_name" VARCHAR(50) NOT NULL,
 	"last_name" VARCHAR(50) NOT NULL,
@@ -24,11 +40,11 @@ CREATE TABLE "user_info" (
 	"updated_at" TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
 
 	PRIMARY KEY ("id"),
-	FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE
+	FOREIGN KEY ("user_id") REFERENCES "db"."user" ("id") ON DELETE CASCADE
 )
 
--- TODO
-CREATE TABLE "toodool" (
+-- TOODOOL
+CREATE TABLE "db"."toodool" (
 	"id" uuid DEFAULT uuid_generate_v4(),
 	"description" VARCHAR(255) UNIQUE NOT NULL,
 	"completed" BOOLEAN DEFAULT false,
@@ -38,5 +54,5 @@ CREATE TABLE "toodool" (
 	"updated_at" TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
 
 	PRIMARY KEY ("id"),
-	FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE
+	FOREIGN KEY ("user_id") REFERENCES "db"."user" ("id") ON DELETE CASCADE
 );
